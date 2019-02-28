20a4b147256750394bcbdaa1e6c297e1766c7c5b
Alexander Forehand <forehand.alex@gmail.com>
2019-02-27 22:18:25 -0800

    Removed connect function, added inline connection object instead.

--- utils/database.py
+++ utils/database.py
@@ -3,46 +3,42 @@ import asyncpg
 from utils.config.config import Config
 
 
-async def db_connect():
-    config = Config()
-    return await asyncpg.connect(database=config.db_name, user=config.db_user, password=config.db_pass)
-
-
 async def db_create():
 
-    # create cursor
-    with db_connect() as connect:
+    # connect to database
+    config = Config()
+    connect = await asyncpg.connect(database=config.db_name, user=config.db_user, password=config.db_pass)
 
-        # create role table
-        await connect.execute('''
-            CREATE TABLE IF NOT EXISTS roles(
-                role_id bigint PRIMARY KEY NOT NULL 
-                );
-            ''')
+    # create role table
+    await connect.execute('''
+             CREATE TABLE IF NOT EXISTS roles(
+                 role_id bigint PRIMARY KEY NOT NULL 
+                 );
+             ''')
 
-        # create permissions table
-        await connect.execute('''
-            CREATE TABLE IF NOT EXISTS permissions(
-                role_id bigint REFERENCES roles(role_id),
-                permission text PRIMARY KEY
-                );
-            ''')
+    # create permissions table
+    await connect.execute('''
+             CREATE TABLE IF NOT EXISTS permissions(
+                 role_id bigint REFERENCES roles(role_id),
+                 permission text PRIMARY KEY
+                 );
+             ''')
 
-        # create users database
-        await connect.execute('''
-            CREATE TABLE IF NOT EXISTS users(
-                user_id bigint PRIMARY KEY
-                );
-            ''')
+    # create users database
+    await connect.execute('''
+             CREATE TABLE IF NOT EXISTS users(
+                 user_id bigint PRIMARY KEY
+                 );
+             ''')
 
-        # create user_role_association
-        await connect.execute('''
-            CREATE TABLE IF NOT EXISTS user_role_association(
-                role_id bigint REFERENCES roles(role_id),
-                user_id bigint PRIMARY KEY
-                );
-            ''')
-
+    # create user_role_association
+    await connect.execute('''
+             CREATE TABLE IF NOT EXISTS user_role_association(
+                 role_id bigint REFERENCES roles(role_id),
+                 user_id bigint PRIMARY KEY
+                 );
+             ''')
+    await connect.close()
 # TODO add role to database
 # TODO add user to database
 # TODO add permission to role
